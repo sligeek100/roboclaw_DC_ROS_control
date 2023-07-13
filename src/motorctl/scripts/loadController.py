d40b925e5ce3f7564d61f2e48e63b61d9f44b345
sli100 <jetshuailee@gmail.com>
2023-07-13 11:09:45 -0400

    cleanedup

--- src/motorctl/scripts/loadController.py
+++ src/motorctl/scripts/loadController.py
@@ -88,35 +88,27 @@ class motorController:
         self.MAX_SPEED = float(rospy.get_param("~max_speed", "1.2"))
         self.TICKS_PER_METER = float(rospy.get_param("~ticks_per_meter", "30921.818"))
         self.NEWTON_PER_MILLIAMP = float(rospy.get_param("~newton_per_milliamp", "0.01"))
-        # self.MAX_POSITION = float(rospy.get_param("~max_position", "20281")) # maximum ticks, i.e., the maximum moving distance of the motor, 20281 -- 0.65m
         self.MAX_POSITION = float(rospy.get_param("~max_position", "18553")) # 0.6m * tm = 18553.0908 ticks, as the new 
         
-        # need the cable length, maximum moving distance is 0.4m.
+        # 
         self.lmax = float(rospy.get_param("~cable_lmax", "0.63"))
         self.lmin = float(rospy.get_param("~cable_lmin", "0.2"))
-        # a subscriber get the reference cmd then write to the roboclaw borad
         rospy.Subscriber("roboclaw/loadReference", motorFlatTarget, self.cmd_load_cb)
 
-
-        # define received reference position, velocity and acc in control class
-        # self.received_position = self.lmax * self.TICKS_PER_METER
         self.received_position = 0 # should be 0, coz 
         self.received_velocity = 0
         self.received_acc = 0
 
-        # a write timer to control the write frequency to roboclaw to avoid the stop working
+        # 
         self.writeFrequency = float(rospy.get_param("~write_frequency", 10)) # 10 hz write is the maximum frequency.
         rospy.Timer(rospy.Duration(1.0/self.writeFrequency), self.writeCmd)
-        # a timer read the motor status and pub out, make it also 10hz, seems it is the board's limits.
+        # a timer
         rospy.Timer(rospy.Duration(0.2), self.pub_motorState_cb)
         self.motorStatePub = rospy.Publisher("roboclaw/load_motor_state", LoadMotorState, queue_size=5)
         
-        # motor control mode, changed by motor service 
+        # motor control mode
         self.mode_list = ['run', 'stop'] # mode lists
         self.ctl_mode = 'run' # string type, default is run
-
-        # a service 
-        # self.setM1Param_srv = rospy.Service("setM1Param", xxx self.setM1Param_srv_cb)
 
         # a service to ser the control mode
         self.setCtlMode_srv =  rospy.Service('roboclaw_stop', Empty, self.ctlMode_cb)
@@ -128,16 +120,9 @@ class motorController:
         rospy.logdebug("max_speed %f", self.MAX_SPEED)
         rospy.logdebug("ticks_per_meter %f", self.TICKS_PER_METER)
         
-    # cmd_load_cb: subs's cb function, get the load reference length.
-    '''
-        max position of the motor (ticks) * ticks/meter = lmax.
-        l_max >= l_max - l_min, so the cmd position is limited within this range.
-    '''
 
     def cmd_load_cb(self, msg):
         
-        # msg is in type LoadFlatTrajectory meter
-        # convert to motor ticks 
         # saturation of the controller 
         if(msg.cable_vel.data >self.MAX_SPEED):
             msg.cable_vel.data = self.MAX_SPEED
@@ -149,13 +134,13 @@ class motorController:
         if(msg.cable_len.data < self.lmin):
             msg.cable_len.data = self.lmin   # lower bound.  lmin < length  < lmax. 
 
-        self.received_position = (self.lmax - msg.cable_len.data) * self.TICKS_PER_METER  # lmax - cmd, because lowest point is 0, highest point is lmax
+        self.received_position = (self.lmax - msg.cable_len.data) * self.TICKS_PER_METER  # lmax
 
         self.received_velocity = abs(msg.cable_vel.data * self.TICKS_PER_METER) # need abs(), it must be a non-negative value
         self.received_acc = abs(msg.cable_acc.data * self.TICKS_PER_METER)
 
     def writeCmd(self, event=None):
-        # a switch like using if else 
+        # 
         if(self.ctl_mode == "run"):
             try:
                 
@@ -169,7 +154,7 @@ class motorController:
 
     '''
     cmd 0 Drive M1
-    send: [address, 0, value, crc(2 bytes)], feceive oxff.
+    send: [address, 0, value, crc(2 bytes)], receive oxff.
     value range is 0-127, 127 =full speed forward, 64 half speed forward, 0 full stop, and this will make the motor stop with force maintained.
     '''
     # check stuff
@@ -215,20 +200,20 @@ class motorController:
         loadState = LoadMotorState()
         
         try:
-            status,enc1, _= self.roboclaw.ReadEncM1(self.address) # enc1 is the motor moving ticks, lmax - tick/tm will be the left length it is the length of the load
+            status,enc1, _= self.roboclaw.ReadEncM1(self.address) # enc1 is the motor moving ticks
             status_v, vel1, _ = self.roboclaw.ReadSpeedM1(self.address)
             status_c, cur1, _ = self.roboclaw.ReadCurrents(self.address)
             status_pwm, pwm1, _ = self.roboclaw.ReadPWMs(self.address)
 
             # convert ticks to m/s etc.
-            motor_position_m = (self.lmax - enc1/self.TICKS_PER_METER) # motor_position_m is the cable length measured 
+            motor_position_m = (self.lmax - enc1/self.TICKS_PER_METER) # 
             motor_velocity_m = -vel1 / self.TICKS_PER_METER
             # fill the msg 
             loadState.header.stamp = rospy.Time.now()
             loadState.header.frame_id = 'map'
             loadState.position = motor_position_m
             loadState.velocity = motor_velocity_m
-            loadState.force = 0.01 * cur1 # approximated force, not correct parameter.
+            loadState.force = 0.01 * cur1 # approximated force, not accurate parameter.
             loadState.pwm = pwm1
             
             self.motorStatePub.publish(loadState)
@@ -242,14 +227,6 @@ class motorController:
             rospy.logwarn("ReadEncM1 OSError: %d", e.errno)
             rospy.logdebug(e)
 
-    # a service to call the set M1 PID constant 
-    # the service cb function.
-    # def setM1Param_srv_cb(self, req): # python server need req as input 
-        # SetM1PositionPID(self,address,kp,ki,kd,ki max,deadzone,min,max)
-        
-    # a service to switch the control mode of the motor controller.
-    # for emergency use to stop the motor spin, can be called from the gui, but will not that efficient.
-    # a client to set the req.mode to 1 is required to stop the motor.
     def ctlMode_cb(self, req):
 
         self.ctl_mode = "stop" # from integar to string. 0 or 1 only
